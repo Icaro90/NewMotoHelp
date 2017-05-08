83cc9ad06b17e0e1bb3d50b6e26b295b282f991a
Thiago <thiago_483@yahoo.com.br>
2017-05-08 19:32:49 -0300

    Thiago Tela de about

--- app/src/main/java/com/example/icaro/newmotohelp/AboutActivity.java
+++ app/src/main/java/com/example/icaro/newmotohelp/AboutActivity.java
@@ -2,7 +2,7 @@ package com.example.icaro.newmotohelp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+/* teste */
 public class AboutActivity extends AppCompatActivity {
 
     @Override
