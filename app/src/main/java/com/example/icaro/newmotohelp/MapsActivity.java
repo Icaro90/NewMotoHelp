49faad99c2f36cd3ab190a5b39d55595d92f84c9
Thiago <thiago_483@yahoo.com.br>
2017-05-08 19:21:41 -0300

    Thiago Tela de about

--- app/src/main/java/com/example/icaro/newmotohelp/MapsActivity.java
+++ app/src/main/java/com/example/icaro/newmotohelp/MapsActivity.java
@@ -1,5 +1,7 @@
 package com.example.icaro.newmotohelp;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -42,5 +44,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-}
+
+
+                    }
+                }
+
+
